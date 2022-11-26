7aa7626cf9bc5ac5c3e8f235135013a68188b598
ParkJuhyun <wngus6429@gmail.com>
2022-11-26 19:20:53 +0900

    update react react-router-dom pathname state

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,14 +1,54 @@
+import { useState } from 'react';
 import { useParams } from 'react-router';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
 interface RouteParams {
   coinId: string;
+}
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto; // 가운데로
+`;
+
+const Header = styled.div`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.div`
+  text-align: center;
+`;
+
+interface RouteState{
+  name:string;
 }
 
 //* useParams가 coinId를 포함하는 오브젝트를 반환할거라고 알려줌
 //* useParams은 URL 정보를 잡아낼수 있게 해줌
 function Coin() {
   const { coinId } = useParams<RouteParams>();
+  const [loading, setLoading] = useState(true);
+  const { state} = useLocation<RouteState>();
+  console.log(state.name)
   console.log(coinId);
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        {/* state가 있으면 name가져오고 아니면 Loading을 보여라 */}
+        <Title>{state?.name || "Loading"}</Title>
+      </Header>
+      {loading ? <Loader>Loading</Loader> : null}
+    </Container>
+  )
 }
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto; // 가운데로
 `;
 
 const Header = styled.div`
@@ -20,9 +23,10 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px; //클릭 범위를 넓혀줌
     transition: color 0.2s ease-in;
-    display: block; // 끝에서 클릭해도 되게끔
   }
   &:hover {
     /* styled 에서 Link는 나중에 a로 바뀌기 때문 */
@@ -37,51 +41,61 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-  {
-    id: 'btc-bitcoin',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'eth-ethereum',
-    name: 'Ethereum',
-    symbol: 'ETH',
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'hex-hex',
-    name: 'HEX',
-    symbol: 'HEX',
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: 'token',
-  },
-];
+const Loader = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([])
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async() => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins')
+      const json = await response.json()
+      setCoins(json.slice(0, 100))
+      setLoading(false);
+    })();
+  },[])
+  // [] 는 컴포넌트 시작점에서만 실행
+  console.log(coins)
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
-        {/* 그냥 a 태그쓰면 새로고침 되어서 Link 사용 */}
-        {/* Link 는 나중에 a태그로 바뀜 */}
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? <Loader>Loading</Loader> : 
+        <CoinsList>
+          {/* 그냥 a 태그쓰면 새로고침 되어서 Link 사용 */}
+          {/* Link 는 나중에 a태그로 바뀜 */}
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={{
+                pathname:`/${coin.id}`,
+                state: {name:coin.name}
+              }}>
+                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} width="50px" />
+                {coin.name} &rarr;
+                </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      }
     </Container>
   );
 }
