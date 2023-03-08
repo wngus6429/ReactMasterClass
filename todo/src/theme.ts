892315701923e52f65cc9369d999f4edbb9311e7
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-03-09 01:23:43 +0900

    update Todo

---
+++ todo/src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  bgColor: 'whitesmoke',
+  textColor: 'black',
+  accentColor: '#9c88ff',
+  cardBgColor: 'white',
+};
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: 'red',
+  accentColor: '#9c88ff',
+  cardBgColor: 'transparent',
+};
