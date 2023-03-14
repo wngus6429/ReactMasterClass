60bc8debefa44ac257aa3301ca068b9bf12009dd
ParkJuhyun <wngus6429@gmail.com>
2023-03-14 19:26:49 +0900

    Update ToDoList.tsx

--- todo/src/ToDoList.tsx
+++ todo/src/ToDoList.tsx
@@ -1,27 +1,27 @@
 // import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-// function ToDoList() {
-//   const [toDo, setToDo] = useState('');
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(toDo);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder='Write a to do' />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
+function ToDoList() {
+  const [toDo, setToDo] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder='Write a to do' />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
 
 interface IForm {
   email: string;
